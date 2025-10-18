44035158565d302279c623c6e1636f30d4913a18
robert <robert.nikolas.torres@gmail.com>
2025-10-18 16:28:23 -0400

    Initial Commit

---
+++ GlanceableFitness Watch App/GlanceableFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlanceableFitnessApp.swift
+//  GlanceableFitness Watch App
+//
+//  Created by Robert Nikolas Torres on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct GlanceableFitness_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
